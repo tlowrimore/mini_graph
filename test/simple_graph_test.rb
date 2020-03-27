925748e90694225c38b3eb1a4db06d3c7ed51fbb
tlowrimore <tlowrimore@coroutine.com>
2020-03-26 22:11:36 -0600

    added more tests

--- test/simple_graph_test.rb
+++ test/simple_graph_test.rb
@@ -1,5 +1,12 @@
 require "test_helper"
 
 class SimpleGraphTest < Minitest::Test
+  test '.create creates a new graph' do
+    graph = SimpleGraph.create %w(A B C D) do
+      directed!
+      edge from: 'A', to: ['B','C','D']
+    end
 
+    assert_equal [1,2,3], graph.adjacent_vertices(0)
+  end
 end
